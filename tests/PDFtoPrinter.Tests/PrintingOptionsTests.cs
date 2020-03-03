c0e8422fdebb620c9a9e5070c517d66147480309
Sergey Vishnevsky <s.vishnevsky@itransition.com>
2020-03-03 14:41:42 +0300

    Fix Printing Options formating

---
+++ tests/PDFtoPrinter.Tests/PrintingOptionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PDFtoPrinter.Tests
+{
+    [TestClass]
+    public class PrintingOptionsTests
+    {
+        [DataTestMethod]
+        [DataRow("printer", "file.path", null, null, null, "\"file.path\" \"printer\"")]
+        [DataRow("printer", "file.path", "1,7-9", null, null, "\"file.path\" \"printer\" pages=1,7-9")]
+        [DataRow("printer", "file.path", null, 12u, null, "\"file.path\" \"printer\" copies=12")]
+        [DataRow("printer", "file.path", null, null, "win", "\"file.path\" \"printer\" focus=\"win\"")]
+        [DataRow("printer", "file.path", "7-9", 2u, "win", "\"file.path\" \"printer\" pages=7-9 copies=2 focus=\"win\"")]
+        public void WhenToStringCalled_ThenReturnFormattedString(
+            string printerName,
+            string filePath,
+            string pages,
+            uint? copies,
+            string focus,
+            string expected)
+        {
+            var options = new PrintingOptions(printerName, filePath)
+            {
+                Copies = copies,
+                Focus = focus,
+                Pages = pages
+            };
+
+            string result = options.ToString();
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ tests/PDFtoPrinter.Tests/StringExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PDFtoPrinter.Tests
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void WhenInputNull_ThenReturnNull()
+        {
+            string input = null;
+
+            string result = input.Format(x => $"formatted {x}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [TestMethod]
+        public void WhenInputEmpty_ThenReturnEmpty()
+        {
+            string input = string.Empty;
+
+            string result = input.Format(x => $"formatted {x}");
+
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void WhenInputNotEmpty_ThenReturnFormatted()
+        {
+            string input = "some string";
+
+            string result = input.Format(x => $"formatted {x}");
+
+            Assert.AreEqual($"formatted {input}", result);
+        }
+    }
+}
