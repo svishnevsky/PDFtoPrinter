502f0281517dc7bfca09a15dbdc4d144ddabc430
Sergey Vishnevsky <s.vishnevsky@itransition.com>
2021-09-27 12:28:18 +0100

    #31 Add ability to wait until file is deleted in CleanupFilesPrinter

--- PDFtoPrinter.Sample/Program.cs
+++ PDFtoPrinter.Sample/Program.cs
@@ -1,13 +1,16 @@
-﻿namespace PDFtoPrinter.Sample
+﻿using System;
+
+namespace PDFtoPrinter.Sample
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            var wrapper = new PDFtoPrinterPrinter();
+            var wrapper = new CleanupFilesPrinter(new PDFtoPrinterPrinter(), true);
             wrapper
                 .Print(new PrintingOptions("Microsoft Print to PDF", "somefile.pdf"))
                 .Wait();
+            Console.ReadKey();
         }
     }
 }
