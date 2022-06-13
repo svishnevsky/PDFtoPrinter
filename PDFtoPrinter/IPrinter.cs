c21f9e9f8f0805d7090317e519ad923dee5c8c23
Sergey Vishnevsky <s.vishnevsky@itransition.com>
2022-06-13 11:45:01 +0100

    #38 allow to run multiple instances of PdfToPrinter.exe
    This was unblocked by adding "m" to the file name as it is hardcoded in the original util

--- PDFtoPrinter/IPrinter.cs
+++ PDFtoPrinter/IPrinter.cs
@@ -6,11 +6,11 @@ namespace PDFtoPrinter
     public interface IPrinter
     {
         /// <summary>
-        /// Runs new PDFtoPrinter.exe process with passed parameters
+        /// Runs new PDFtoPrinter_m.exe process with passed parameters
         /// </summary>
         /// <param name="options">Options to a printer util.</param>
         /// <param name="timeout">
-        /// Printing timeout. If PDFtoPrinter.exe process isn't exited after this timeout, 
+        /// Printing timeout. If PDFtoPrinter_m.exe process isn't exited after this timeout, 
         /// the process will be killed. Default value is 1 minute.
         /// </param>
         /// <returns>Asynchronous task.</returns>
