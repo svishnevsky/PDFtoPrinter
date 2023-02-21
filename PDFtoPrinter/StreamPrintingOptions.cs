2bd1c63e5acae25203ec606efc34067547db0c94
Sergey Vishnevsky <s.vishnevsky@indigina.com>
2023-02-21 10:35:20 +0000

    #43 add an extension to print stream
    Stream saves to a temp file before printing

---
+++ PDFtoPrinter/StreamPrintingOptions.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace PDFtoPrinter
+{
+    /// <summary>
+    /// Options for a Printer
+    /// </summary>
+    public class StreamPrintingOptions : PrintingOptions
+    {
+        private const string FileExtension = "pdf";
+
+        /// <summary>
+        /// Creates new <see cref="StreamPrintingOptions"/> instance.
+        /// </summary>
+        /// <param name="printerName">Name of the printer.</param>
+        public StreamPrintingOptions(string printerName)
+            : base(
+                printerName,
+                Path.ChangeExtension(
+                    Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()),
+                    FileExtension))
+        {
+        }
+    }
+}
